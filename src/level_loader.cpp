9531f8456d7d57a424995cfe62b64f958fe5fb8b
geemili <minergeemili@gmail.com>
2016-07-05 23:53:32 -0500

    Added in next level

--- src/level_loader.cpp
+++ src/level_loader.cpp
@@ -2,6 +2,24 @@
 #include "level_loader.h"
 #include "levels.h"
 
+#define CHAR_WALL '#'
+#define CHAR_EXIT '!'
+#define CHAR_PLAYER '@'
+
+#define CHAR_CRATE_V 'k'
+#define CHAR_CRATE_H 'l'
+#define CHAR_CRATE_S 'm'
+
+#define CHAR_SLOT_V0 'a'
+#define CHAR_SLOT_V1 'b'
+#define CHAR_SLOT_V2 'c'
+#define CHAR_SLOT_V3 'd'
+
+#define CHAR_SLOT_H0 'f'
+#define CHAR_SLOT_H1 'g'
+#define CHAR_SLOT_H2 'h'
+#define CHAR_SLOT_H3 'i'
+
 Level *load_level(uint8_t level_num) {
   PGM_P level_address = get_level_address(level_num);
   // TODO: Make use of this. Ignoring length check right now
@@ -15,14 +33,17 @@ Level *load_level(uint8_t level_num) {
       int tile_index = (j * w) + i;
       char c = pgm_read_byte_near(level_address + 2 + tile_index);
       switch (c) {
-        case '#':
+        case CHAR_WALL:
           level->walls[tile_index] = true;
           break;
-        case '!':
+        case CHAR_EXIT:
           level->exit_pos = Pos{i, j};
           break;
-        case '@':
+        case CHAR_PLAYER:
           level->player_pos = Pos{i, j};
+          break;
+        case CHAR_CRATE_V:
+          level->add_crate(new Crate{Pos{i, j}, shapes::CRATE_VERTICAL});
           break;
         default: break; // Ignore everything else
       }
--- src/levels.h
+++ src/levels.h
@@ -11,9 +11,17 @@ const PROGMEM char LEVEL_00[] =
 #@....!#\
 ########";
 
+const PROGMEM char LEVEL_01[] =
+"\x08\x04\
+........\
+########\
+#@..k.!#\
+########";
+
 PGM_P get_level_address(uint8_t num) {
   switch (num) {
     case 0x00: return LEVEL_00;
+    case 0x01: return LEVEL_01;
   }
   return LEVEL_00;
 }
