c7213f35add58ffbd9e164217ec97db18ed939d3
geemili <minergeemili@gmail.com>
2016-07-20 11:36:03 -0500

    Got it working! I found out what the missing piece was.
    Every time you start a connection with the oled, you have to tell if you're in command mode or data mode. I was telling it that I was in data mode, but I never bothered to tell that I was sending commands. :D

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@
 #define BLINK_DELAY_MS 1000
 
 void setup_i2c();
-void clear_display(int16_t x, int16_t y, int16_t w, int16_t h);
-void draw_buffer();
+void clear_display();
+void draw_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t color);
 
 int main (void)
 {
@@ -29,10 +29,12 @@ int main (void)
     setup_i2c();
   }
 
+  clear_display();
   int16_t x = 0;
   int16_t y = 0;
   while (1) {
-    clear_display(x, y, 5, 6);
+    // clear_display(0, 0, 0, 0);
+    draw_rect(x, y, 5, 6, 0);
     y++;
     if (y > 64) {
       x++;
@@ -41,11 +43,125 @@ int main (void)
     if (x > 128) {
       x = 0;
     }
+    draw_rect(x, y, 5, 6, 1);
   }
+}
+
+void clear_display() {
+  i2c_start(OLED_ADDRESS + I2C_WRITE);
+  i2c_write(0x00); // Command mode
+
+  i2c_write(0x21); // Column
+  i2c_write(0);    // start
+  i2c_write(127);    // end
+
+  i2c_write(0x22); // Page address / row adress
+  i2c_write(0x0); // start
+  i2c_write(0x7);
+
+  i2c_stop();
+
+  unsigned char ret;
+  uint8_t num_transmitted = 0;
+  for (uint16_t page=0; page<8; page++) {
+    for (uint16_t col=0; col<128; col++) {
+      if (num_transmitted==0) {
+        ret = i2c_start(OLED_ADDRESS + I2C_WRITE);
+        i2c_write(0x40); // Data Mode
+      }
+      if (ret) {
+        i2c_stop();
+        PORTB |= _BV(PORTB5);
+      } else {
+        i2c_write(0x00);
+        num_transmitted++;
+        if (num_transmitted==16) {
+          i2c_stop();
+          num_transmitted = 0;
+        }
+      }
+    }
+  }
+
+  if (num_transmitted != 0) i2c_stop();
+}
+
+void draw_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t color) {
+  if (x >= 128 || y >= 64) return;
+
+  uint8_t start_col = x,
+          end_col = 127,
+          start_page = y>>3,
+          end_page = 7;
+
+  if (x+w < 128) {
+    end_col = x+w;
+  }
+
+  if (y+h < 64) {
+    end_page = (y+h)>>3;
+  }
+
+  // uint8_t twbr_prev = TWBR;
+  // TWBR = 12;
+
+  unsigned char ret;
+  uint8_t num_transmitted = 0;
+  uint8_t open = 0;
+  for (uint16_t page=start_page; page<=end_page; page++) {
+    if (open) {
+      i2c_stop();
+      open = 0;
+      num_transmitted = 0;
+    }
+    i2c_start(OLED_ADDRESS + I2C_WRITE);
+    i2c_write(0x00); // Command mode
+
+    i2c_write(0x21); // Column
+    i2c_write(start_col);    // start
+    i2c_write(end_col);
+
+    i2c_write(0x22); // Page address / row adress
+    i2c_write(page); // start
+    i2c_write(page);
+
+    i2c_stop();
+    for (uint16_t col=start_col; col<end_col; col++) {
+      if (num_transmitted==0) {
+        open = 1;
+        ret = i2c_start(OLED_ADDRESS + I2C_WRITE);
+        i2c_write(0x40); // Data mode
+      }
+      if (ret) {
+        i2c_stop();
+        open = 0;
+        PORTB |= _BV(PORTB5);
+      } else {
+        uint8_t segment = 0x00;
+        for (uint8_t i=0; i<8; i++) {
+          if (((page*8 + i) >= y) && ((page*8 + i) < (y+h))) {
+            segment |= (color << i);
+          }
+        }
+        i2c_write(segment);
+        num_transmitted++;
+        if (num_transmitted==16) {
+          i2c_stop();
+          open = 0;
+          num_transmitted = 0;
+        }
+      }
+    }
+  }
+
+  if (open) i2c_stop();
+
+  // TWBR = twbr_prev;
 }
 
 void setup_i2c() {
   i2c_start(OLED_ADDRESS + I2C_WRITE);
+  i2c_write(0x00); // Command mode
 
   // Setup the OLED screen
   i2c_write(0xAE); // Turn the display off
@@ -66,7 +182,7 @@ void setup_i2c() {
   i2c_write(63); // 1/64 duty cycle
 
   i2c_write(0xD3); // Set display offset to...
-  i2c_write(0x0);  // nothing
+  i2c_write(0x1);  // 1. If it is left at 0 it is offset by 1
 
   i2c_write(0xD5); // Set display clk
   i2c_write(0x80);
@@ -92,77 +208,4 @@ void setup_i2c() {
   i2c_write(0xAF);
 
   i2c_stop();
-}
-
-void draw_buffer() {
-  i2c_start(OLED_ADDRESS + I2C_WRITE);
-
-  i2c_write(0x21); // Column
-  i2c_write(0);    // start
-  i2c_write(127);    // end
-
-  i2c_write(0x22); // Page address / row adress
-  i2c_write(0); // start
-  i2c_write(7);
-
-  i2c_stop();
-
-
-  for (uint16_t i=0; i < 127; i++) {
-    unsigned char ret = i2c_start(OLED_ADDRESS + I2C_WRITE);
-    if (ret) {
-      i2c_stop();
-      PORTB |= _BV(PORTB5);
-    } else {
-      i2c_write(0x40);
-      i2c_write(i & 0xFF);
-      i2c_stop();
-    }
-  }
-}
-
-void clear_display(int16_t x, int16_t y, int16_t w, int16_t h) {
-  i2c_start(OLED_ADDRESS + I2C_WRITE);
-
-  i2c_write(0x21); // Column
-  i2c_write(0);    // start
-  i2c_write(127);    // end
-
-  i2c_write(0x22); // Page address / row adress
-  i2c_write(0x0); // start
-  i2c_write(0x7);
-
-  i2c_stop();
-
-  unsigned char ret;
-  uint8_t num_transmitted = 0;
-  for (uint16_t page=0; page<7; page++) {
-    for (uint16_t col=0; col<128; col++) {
-      if (num_transmitted==0) {
-        ret = i2c_start(OLED_ADDRESS + I2C_WRITE);
-        i2c_write(0x40);
-      }
-      if (ret) {
-        i2c_stop();
-        PORTB |= _BV(PORTB5);
-      } else {
-        uint8_t segment = 0x00;
-        if (col >= x && col < x+w) {
-          if (y>>3 == page || (y+h)>>3 == page) {
-            for (uint8_t i=0; i<8; i++) {
-              if (page*8 + i >= y && page*8 + i < y+h) {
-                segment |= (1 << i);
-              }
-            }
-          }
-        }
-        i2c_write(segment);
-        num_transmitted++;
-        if (num_transmitted==16) {
-          i2c_stop();
-          num_transmitted = 0;
-        }
-      }
-    }
-  }
 }
